da602b6027fc8b284611ea58a9cff7681cb33165
Nathalie <fillon_1@hotmail.com>
2023-05-21 11:51:13 -0400

    dernière phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import { BACKDROP_BASE_URL } from "./config";
+import s from "./style.module.css";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+
 export function App() {
-  return <div>Hello World!! </div>;
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo here</div>
+            <div>subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommended_shows}>Recommended tv shows</div>
+    </div>
+  );
 }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=53165f7eb0980de90008d3ad6fd15100";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL};
