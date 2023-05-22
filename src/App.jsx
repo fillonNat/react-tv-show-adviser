8f976407a23a28a85ec22d4563b7ee6dd078b680
Nathalie <fillon_1@hotmail.com>
2023-05-22 10:54:48 -0400

    derniere publication

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,59 @@ import { TVShowAPI } from "./api/tv-show";
 import { BACKDROP_BASE_URL } from "./config";
 import s from "./style.module.css";
 import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logo from "./assets/images/logo.png";
+import { TVShowList } from "./components/TVShowList/TVShowList";
+import { SearchBar } from "./components/SearchBar/SearchBar";
 
 export function App() {
   const [currentTVShow, setCurrentTVShow] = useState();
+  const [recommendationList, setRecommendationList] = useState([]);
 
   async function fetchPopulars() {
-    const populars = await TVShowAPI.fetchPopulars();
-    if (populars.length > 0) {
-      setCurrentTVShow(populars[0]);
+    try {
+      const populars = await TVShowAPI.fetchPopulars();
+      if (populars.length > 0) {
+        setCurrentTVShow(populars[0]);
+      }
+    } catch (error) {
+      alert(
+        "Erreur durant la recherche des séries populaires " + error.message
+      );
     }
   }
+
+  async function fetchRecommendations(tvShowId) {
+    try {
+      const recommendations = await TVShowAPI.fetchRecommendations(tvShowId);
+      if (recommendations.length > 0) {
+        setRecommendationList(recommendations.slice(0, 10));
+      }
+    } catch (error) {
+      alert("Erreur durant la recherche des séries recommendées");
+    }
+  }
+
   useEffect(() => {
     fetchPopulars();
   }, []);
 
+  useEffect(() => {
+    if (currentTVShow) {
+      fetchRecommendations(currentTVShow.id);
+    }
+  }, [currentTVShow]);
+
+  async function searchTVShow(tvShowName) {
+    try {
+      const searchResponse = await TVShowAPI.fetchByTitle(tvShowName);
+      if (searchResponse.length > 0) {
+        setCurrentTVShow(searchResponse[0]);
+      }
+    } catch (error) {
+      alert("Erreur durant la recherche de la série ");
+    }
+  }
   return (
     <div
       className={s.main_container}
@@ -29,18 +68,28 @@ export function App() {
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
-            <div>Logo here</div>
-            <div>subtitle</div>
+            <Logo
+              image={logo}
+              title="Watowatch"
+              subtitle="Find a show you may like"
+            />
           </div>
           <div className="col-md-12 col-lg-4">
-            <input style={{ width: "100%" }} type="text" />
+            <SearchBar onSubmit={searchTVShow} />
           </div>
         </div>
       </div>
       <div className={s.tv_show_details}>
         {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
       </div>
-      <div className={s.recommended_shows}>Recommended tv shows</div>
+      <div className={s.recommended_shows}>
+        {recommendationList && recommendationList.length > 0 && (
+          <TVShowList
+            onClickItem={setCurrentTVShow}
+            tvShowList={recommendationList}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_PARAM = "?api_key=53165f7eb0980de90008d3ad6fd15100";
-const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/"
 
-export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL};
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL};
